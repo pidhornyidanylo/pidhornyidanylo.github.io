82cbec2dd1617daebd804d96a588079c82b27c6e
Danylo Pidhornyi <105637978+pidhornyidanylo@users.noreply.github.com>
2023-01-01 17:30:29 +0200

    Add files via upload

---
+++ rotatingContent/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const close = document.getElementById('close');
+    const open = document.getElementById('open');
+
+    open.addEventListener('click', () => {
+        container.classList.add('show-nav');
+    });
+
+    close.addEventListener('click', () => {
+        container.classList.remove('show-nav');
+    });
+
+});
